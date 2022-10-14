c30c8f53f878db723d8cfa4c2855c9f0169edbb9
EMatsonE <90651995+EMatsonE@users.noreply.github.com>
2022-10-14 11:50:43 -0400

    Add project files.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Giraffe
+{
+    //class is a container where we put code
+    class Program
+    {
+        ////this is the starting part of my code
+        ////the code will read line by line, so the order matters a lot
+        //static void Main(string[] args)
+        //{    
+        //    //variables are amazing because they can store a specific data value we can use at any time
+        //    //if you change what's in the variable you change what appears in the code
+        //    string characterName = "John";
+        //    //below is a different way of asigning something to a variable
+        //    int characterAge;
+        //    characterAge = 35;
+        //    //console.writeline is an instruction to the console
+        //    Console.WriteLine("There once was a man named " + characterName);
+        //    //below is an exaple on concatonating of a string
+
+        //    Console.WriteLine("He was " + characterAge + "years old");
+        //    //you can change the value halfway through the code
+        //    characterName = "Mike";
+
+        //    Console.WriteLine("He really liked the name " + characterName);
+        //    Console.WriteLine("But didn't like being " + characterAge);
+        //    //without readline it will close
+        //    Console.ReadLine();
+        //    //to close the console hit enter or 'x' out of it
+        //}
+        //static void Main(string[] args)
+        ////data structures
+        //{
+        //    string phrase = "Giraffe Attack";
+        //    char grade = 'S';
+        //    int age = 30;
+        //    //decimal can be stored using these three structures going from least accurate to most accurate. float, double, decimal
+        //    double gpa = 3.3;
+        //    //boolean is a true or false value
+        //    bool isMale = true;
+
+
+
+        //    Console.ReadLine();
+        //}
+        //static void Main(string[] args)
+        ////woring with strings
+        //{
+        //    // the use of \n will make a new line where as \"word\" will let you print out actual quotations
+        //    //concatonation is adding 2 strings together
+        //    string phrase = "Giraffe\n\"Academy\"" + "is pretty neat";
+        //    //phrase.Length will print out the ammount of characters used
+        //    //phrase.ToUpper() will make it caps
+        //    //phrase.ToLower() will make it lower case
+        //    //phrase.Contains("Giraffe") will have it print true because the string contains that word
+        //    //phrase[0] will print out the first character
+        //    //all indexes start with 0 unless otherwise specified
+        //    //phrase.IndexOf("Academy") will tell you what index number Academy starts at at
+        //    //if you put a character in .IndexOf() like z then you will get -1 to indicate that the index doesn't have that number
+        //    //phrase.Substring(8) will only print out the string starting at that number
+        //    //phrase.Substring(8,11) will only print out the chracters starting at 8 and ending at 11
+        //    //google C# string methods to see other ways you can mess with strings
+        //    Console.WriteLine(phrase);
+
+        //    Console.ReadLine();
+
+        //}
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
